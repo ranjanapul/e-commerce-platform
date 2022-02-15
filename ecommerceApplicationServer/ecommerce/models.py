8097b6f08bdc788ba7def8ea0b395ef6ef7840eb
Apul <ranjanapul@gmail.com>
2022-02-15 21:45:40 +0530

    Created user models and made database entries

--- ecommerceApplicationServer/ecommerce/models.py
+++ ecommerceApplicationServer/ecommerce/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import datetime
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+class User(models.Model):
+    userId = models.AutoField(primary_key=True)
+    CUSTOMER = 'Customer'
+    VENDOR = 'Vendor'
+    CHOICES = ((CUSTOMER, 'Customer'), (VENDOR, 'Vendor'))
+    userType = models.CharField(max_length=10, choices=CHOICES)
+    name = models.CharField(max_length=40)
+    address = models.CharField(max_length=100)
+    phoneNumber = models.CharField(max_length=10,
+                                   validators=[MinLengthValidator(10)])
+    email = models.CharField(max_length=30)
+    status = models.BooleanField()
+    createdTs = models.DateTimeField(default=datetime.now, blank=True)
+    # modifiedTs = models.DateTimeField(auto_now=True)
