4cd2bc3d9c96cf4f29f225fa20c2448b0fffeb0e
Apul <ranjanapul@gmail.com>
2022-02-23 00:41:23 +0530

    Added postman collection

--- ecommerceApplicationServer/ecommerceApplicationServer/settings.py
+++ ecommerceApplicationServer/ecommerceApplicationServer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ssshtxf4%%h$)s7j*gb3ux*=t*468tfzvhtv$y+a(p2m-lt4e'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
