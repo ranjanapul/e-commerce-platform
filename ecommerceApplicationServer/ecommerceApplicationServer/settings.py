380bab01fde5c0999395e9afd3ed4b6a109c9908
Apul <ranjanapul@gmail.com>
2022-04-12 23:16:50 +0530

    connected postgres

--- ecommerceApplicationServer/ecommerceApplicationServer/settings.py
+++ ecommerceApplicationServer/ecommerceApplicationServer/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ecommerceApplicationServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecommercedb', 
+        'USER': 'postgres', 
+        'PASSWORD': 'nimbus@ar83',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
